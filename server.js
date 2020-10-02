21d0f4911e2562dd0a635d81df5725934afb056c
Elizabeth Lee <dlekwjd347@gmail.com>
2020-10-01 21:19:25 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_ATLAS_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/apiroutes.js"));
